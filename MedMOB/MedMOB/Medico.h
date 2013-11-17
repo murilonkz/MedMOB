7881bd4e29d649210583f3f06d7eec94ca7a914d
Thiago Oliveira <thiagoariolimendes@gmail.com>
2013-11-17 18:01:02 -0200

    criado medico

---
+++ MedMOB/MedMOB/Medico.h
@@ -0,0 +1,13 @@
+//
+//  Medico.h
+//  MedMOB
+//
+//  Created by Thiago Oliveira on 11/17/13.
+//  Copyright (c) 2013 Murilo Campaner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Medico : NSObject
+
+@end

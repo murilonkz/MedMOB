1aa2da4fc5f56bf9a059977cb4b89671dc6b27b4
Murilo Campaner <murilocampaner@MacBook-Pro-de-Murilo.local>
2013-11-17 17:52:24 -0200

    First Commit

---
+++ MedMOB/MedMOB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MedMOB
+//
+//  Created by Murilo Campaner on 17/11/13.
+//  Copyright (c) 2013 Murilo Campaner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
